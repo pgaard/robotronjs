fa52f98c067ff36e5bcd6c571cb735633a42d0ef
pgaard <pgx00@yahoo.com>
2015-02-11 22:52:32 -0600

    tank shots bounce off walls

--- Scripts/game/sprites/tankshot.js
+++ Scripts/game/sprites/tankshot.js
@@ -8,6 +8,7 @@ var TankShot = (function (_super) {
     __extends(TankShot, _super);
     function TankShot(game, left, top, shootX, shootY, rgbColors) {
         _super.call(this, 'tankshot', game, left, top);
+        this.dissapearing = false;
         this.velocityX = shootX * TankShot.bulletSpeed;
         this.velocityY = shootY * TankShot.bulletSpeed;
         this.canKill = true;
@@ -17,20 +18,51 @@ var TankShot = (function (_super) {
     TankShot.prototype.painter = function (context) {
         context.save();
         context.beginPath();
-        context.arc(this.left + TankShot.radius, this.top - TankShot.radius, TankShot.radius, 0, Math.PI * 2, false);
+        context.arc(this.left + TankShot.radius, this.top + TankShot.radius, TankShot.radius, 0, Math.PI * 2, false);
         context.fillStyle = RobotronSprite.rgbColors();
         context.lineWidth = 2;
         context.strokeStyle = 'white';
         context.fill();
         context.stroke();
+        context.beginPath();
+        context.arc(this.left + TankShot.radius, this.top + TankShot.radius, TankShot.radius - 2, 0, Math.PI * 2, false);
+        context.strokeStyle = 'black';
+        context.stroke();
         context.restore();
     };
     TankShot.prototype.mover = function (context, time) {
         this.move(time);
+        this.fireRandomEvents();
+    };
+    TankShot.prototype.checkDissapear = function () {
+        var _this = this;
+        if (!this.dissapearing) {
+            this.dissapearing = true;
+            this.queueRandomEvent(2, .5, false, function () {
+                _this.game.removeSprite(_this);
+            });
+        }
     };
     TankShot.prototype.adjustMoveDelta = function (deltaX, deltaY) {
-        if (this.left + deltaX > this.game.right || this.left + deltaX < this.game.left || this.top + deltaY > this.game.bottom || this.top + deltaY < this.game.top) {
-            this.game.removeSprite(this);
+        if (this.left + deltaX + this.width > this.game.right) {
+            this.velocityX = -Math.abs(this.velocityX);
+            deltaX = -Math.abs(deltaX);
+            this.checkDissapear();
+        }
+        else if (this.left + deltaX < this.game.left) {
+            this.velocityX = Math.abs(this.velocityX);
+            deltaX = Math.abs(deltaX);
+            this.checkDissapear();
+        }
+        else if (this.top + deltaY + this.height > this.game.bottom) {
+            this.velocityY = -Math.abs(this.velocityY);
+            deltaY = -Math.abs(deltaY);
+            this.checkDissapear();
+        }
+        else if (this.top + deltaY < this.game.top) {
+            this.velocityY = Math.abs(this.velocityY);
+            deltaY = Math.abs(deltaY);
+            this.checkDissapear();
         }
         return { deltaX: deltaX, deltaY: deltaY };
     };
@@ -40,7 +72,7 @@ var TankShot = (function (_super) {
         //TODO: sound
         this.game.removeSprite(this);
     };
-    TankShot.bulletSpeed = 400;
+    TankShot.bulletSpeed = 500;
     TankShot.radius = 10;
     return TankShot;
 })(RobotronSprite);
--- Scripts/game/sprites/tankshot.ts
+++ Scripts/game/sprites/tankshot.ts
@@ -1,14 +1,15 @@
-﻿ class TankShot extends RobotronSprite {
+﻿class TankShot extends RobotronSprite {
     velocityX: number;
     velocityY: number;
-    static bulletSpeed = 400;
+    dissapearing = false;
+    static bulletSpeed = 500;
     static radius = 10;
 
-    constructor(game: Game, left: number, top: number, shootX: number, shootY: number, rgbColors: () => string ) {
+    constructor(game: Game, left: number, top: number, shootX: number, shootY: number, rgbColors: () => string) {
         super('tankshot', game, left, top);
         this.velocityX = shootX * TankShot.bulletSpeed;
         this.velocityY = shootY * TankShot.bulletSpeed;
-        this.canKill = true;    
+        this.canKill = true;
         this.enemy = true;
         this.width = this.height = TankShot.radius * 2;
     }
@@ -16,33 +17,64 @@
     painter(context: CanvasRenderingContext2D) {
         context.save();
         context.beginPath();
-        context.arc(this.left + TankShot.radius, this.top - TankShot.radius, TankShot.radius, 0, Math.PI * 2, false);
+        context.arc(this.left + TankShot.radius, this.top + TankShot.radius, TankShot.radius, 0, Math.PI * 2, false);
         context.fillStyle = RobotronSprite.rgbColors();
         context.lineWidth = 2;
         context.strokeStyle = 'white';
         context.fill();
         context.stroke();
+
+        context.beginPath();
+        context.arc(this.left + TankShot.radius, this.top + TankShot.radius, TankShot.radius - 2, 0, Math.PI * 2, false);
+        context.strokeStyle = 'black';
+        context.stroke();
+
         context.restore();
     }
 
     mover(context: CanvasRenderingContext2D, time: number) {
         this.move(time);
+        this.fireRandomEvents();    
+    }
+
+    checkDissapear() {
+        if (!this.dissapearing) {
+            this.dissapearing = true;
+            this.queueRandomEvent(2, .5, false, () => {
+                this.game.removeSprite(this)
+            });
+        }
     }
 
     adjustMoveDelta(deltaX: number, deltaY: number) {
-        if (this.left + deltaX > this.game.right ||
-            this.left + deltaX < this.game.left ||
-            this.top + deltaY > this.game.bottom ||
-            this.top + deltaY < this.game.top) {
-            this.game.removeSprite(this);
+
+        if (this.left + deltaX + this.width > this.game.right) {
+            this.velocityX = -Math.abs(this.velocityX);
+            deltaX = -Math.abs(deltaX);
+            this.checkDissapear();
+        }
+        else if (this.left + deltaX < this.game.left) {
+            this.velocityX = Math.abs(this.velocityX);
+            deltaX = Math.abs(deltaX);
+            this.checkDissapear();
+        }
+        else if (this.top + deltaY + this.height > this.game.bottom) {
+            this.velocityY = -Math.abs(this.velocityY);
+            deltaY = -Math.abs(deltaY);
+            this.checkDissapear();
+        }
+        else if (this.top + deltaY < this.game.top) {
+            this.velocityY = Math.abs(this.velocityY);
+            deltaY = Math.abs(deltaY);
+            this.checkDissapear();
         }
         return { deltaX: deltaX, deltaY: deltaY };
-    }  
+    }
 
-     kill(bullet: Bullet) {
-         var horizontal = Math.abs(bullet.velocityY) > Math.abs(bullet.velocityX);
-         var explosion = new Explosion(bullet.game, this.left, this.top, this.width, this.height, horizontal);
-         //TODO: sound
+    kill(bullet: Bullet) {
+        var horizontal = Math.abs(bullet.velocityY) > Math.abs(bullet.velocityX);
+        var explosion = new Explosion(bullet.game, this.left, this.top, this.width, this.height, horizontal);
+        //TODO: sound
         this.game.removeSprite(this);
     }
 }
