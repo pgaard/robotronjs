c35b933a51697d6f0251ce2f3c8e709e5028afb6
pgaard <pgx00@yahoo.com>
2015-02-17 22:55:13 -0600

    use larger text font

--- Scripts/game/textwriter.js
+++ Scripts/game/textwriter.js
@@ -2,32 +2,54 @@ var TextWriter = (function () {
     function TextWriter(context, spriteSheet) {
         this.context = context;
         this.spriteSheet = spriteSheet;
-        if (!TextWriter.images) {
-            TextWriter.images = [];
+        if (!TextWriter.smallCells) {
+            TextWriter.smallCells = [];
             for (var i = 0; i < 25; i++) {
                 var extra = (i > 22) ? 4 : 0;
-                TextWriter.images.push({
-                    x: TextWriter.spriteStartPosRow1.x + i * TextWriter.spriteIntervalX + extra,
-                    y: TextWriter.spriteStartPosRow1.y,
+                TextWriter.smallCells.push({
+                    x: TextWriter.smallStartPosRow1.x + i * TextWriter.smallIntervalX + extra,
+                    y: TextWriter.smallStartPosRow1.y,
                     w: i == 22 ? 10 : 6,
-                    h: 10
+                    h: TextWriter.smallSize.y
                 });
             }
             for (var i = 0; i < 11; i++) {
                 var extra = (i > 7) ? 4 : 0;
-                TextWriter.images.push({
-                    x: TextWriter.spriteStartPosRow2.x + i * TextWriter.spriteIntervalX + extra,
-                    y: TextWriter.spriteStartPosRow2.y,
+                TextWriter.smallCells.push({
+                    x: TextWriter.smallStartPosRow2.x + i * TextWriter.smallIntervalX + extra,
+                    y: TextWriter.smallStartPosRow2.y,
                     w: i == 7 ? 10 : 6,
-                    h: 10
+                    h: TextWriter.smallSize.y
                 });
             }
-            TextWriter.images.push({ x: 346, y: 164, w: 15, h: 22 });
+            TextWriter.smallCells.push({ x: 346, y: 164, w: 15, h: 22 });
+        }
+        if (!TextWriter.largeCells) {
+            TextWriter.largeCells = [];
+            for (var i = 0; i < 17; i++) {
+                TextWriter.largeCells.push({
+                    x: TextWriter.largeStartPosRow1.x + i * TextWriter.largeIntervalX,
+                    y: TextWriter.largeStartPosRow1.y,
+                    w: TextWriter.largeSize.x,
+                    h: TextWriter.largeSize.y
+                });
+            }
+            for (var i = 0; i < 19; i++) {
+                TextWriter.largeCells.push({
+                    x: TextWriter.largeStartPosRow2.x + i * TextWriter.largeIntervalX,
+                    y: TextWriter.largeStartPosRow2.y,
+                    w: TextWriter.largeSize.x,
+                    h: TextWriter.largeSize.y
+                });
+            }
+            TextWriter.largeCells.push({ x: 346, y: 164, w: 15, h: 22 });
         }
     }
-    TextWriter.prototype.write = function (text, x, y, scale) {
+    TextWriter.prototype.write = function (text, x, y, scale, large) {
         if (scale === void 0) { scale = 1; }
+        if (large === void 0) { large = true; }
         var index;
+        var cells = large ? TextWriter.largeCells : TextWriter.smallCells;
         var pos = 0;
         for (var i = 0; i < text.length; i++) {
             var space = false;
@@ -47,15 +69,19 @@ var TextWriter = (function () {
                 pos += (6 * scale);
                 continue;
             }
-            var image = TextWriter.images[index];
+            var image = cells[index];
             this.context.drawImage(this.spriteSheet, image.x, image.y, image.w, image.h, x + pos, y, image.w * imageScale, image.h * imageScale);
             pos += ((image.w + 2) * imageScale);
         }
     };
-    TextWriter.spriteStartPosRow1 = { x: 153, y: 275 };
-    TextWriter.spriteStartPosRow2 = { x: 1, y: 317 };
-    TextWriter.spriteIntervalX = 18;
-    TextWriter.spriteSize = { x: 6, y: 10 };
+    TextWriter.smallStartPosRow1 = { x: 153, y: 275 };
+    TextWriter.smallStartPosRow2 = { x: 1, y: 317 };
+    TextWriter.smallIntervalX = 18;
+    TextWriter.smallSize = { x: 6, y: 10 };
+    TextWriter.largeStartPosRow1 = { x: 239, y: 317 };
+    TextWriter.largeStartPosRow2 = { x: 1, y: 339 };
+    TextWriter.largeIntervalX = 22;
+    TextWriter.largeSize = { x: 10, y: 12 };
     return TextWriter;
 })();
 //# sourceMappingURL=textwriter.js.map
--- Scripts/game/textwriter.ts
+++ Scripts/game/textwriter.ts
@@ -1,40 +1,71 @@
 ﻿class TextWriter {
-    static spriteStartPosRow1 = { x: 153, y: 275 }; 
-    static spriteStartPosRow2 = { x: 1, y: 317 }; 
-    static spriteIntervalX = 18;
-    static spriteSize = { x: 6, y: 10 };
-    static images: { x: number; y: number; w: number; h: number }[];
+    static smallStartPosRow1 = { x: 153, y: 275 }; 
+    static smallStartPosRow2 = { x: 1, y: 317 }; 
+    static smallIntervalX = 18;
+    static smallSize = { x: 6, y: 10 };
+    static smallCells: { x: number; y: number; w: number; h: number }[];
+
+    static largeStartPosRow1 = { x: 239, y: 317 };
+    static largeStartPosRow2 = { x: 1, y: 339 };
+    static largeIntervalX = 22;
+    static largeSize = { x: 10, y: 12 };
+    static largeCells: { x: number; y: number; w: number; h: number }[];
 
     constructor(private context: CanvasRenderingContext2D, private spriteSheet: HTMLImageElement) {
-        if (!TextWriter.images) {
-            TextWriter.images = [];
+        if (!TextWriter.smallCells) {
+            TextWriter.smallCells = [];
             // 0 - 9 a - o
             for (var i = 0; i < 25; i++) {
                 var extra = (i > 22) ? 4 : 0
-                TextWriter.images.push({
-                    x: TextWriter.spriteStartPosRow1.x + i * TextWriter.spriteIntervalX + extra,
-                    y: TextWriter.spriteStartPosRow1.y,
+                TextWriter.smallCells.push({
+                    x: TextWriter.smallStartPosRow1.x + i * TextWriter.smallIntervalX + extra,
+                    y: TextWriter.smallStartPosRow1.y,
                     w: i == 22 ? 10 : 6,
-                    h: 10
+                    h: TextWriter.smallSize.y
                 });
             }
             // p - z
             for (var i = 0; i < 11; i++) {
                 var extra = (i > 7) ? 4 : 0
-                TextWriter.images.push({
-                    x: TextWriter.spriteStartPosRow2.x + i * TextWriter.spriteIntervalX + extra,
-                    y: TextWriter.spriteStartPosRow2.y,
+                TextWriter.smallCells.push({
+                    x: TextWriter.smallStartPosRow2.x + i * TextWriter.smallIntervalX + extra,
+                    y: TextWriter.smallStartPosRow2.y,
                     w: i == 7 ? 10 : 6,
-                    h: 10
+                    h: TextWriter.smallSize.y
                 });
             }
 
-            TextWriter.images.push({x: 346, y: 164, w: 15, h: 22});
+            TextWriter.smallCells.push({x: 346, y: 164, w: 15, h: 22});
+        }
+
+        if (!TextWriter.largeCells) {
+            TextWriter.largeCells = [];
+            // 0 - 9 a - o
+            for (var i = 0; i < 17; i++) {
+                TextWriter.largeCells.push({
+                    x: TextWriter.largeStartPosRow1.x + i * TextWriter.largeIntervalX ,
+                    y: TextWriter.largeStartPosRow1.y,
+                    w: TextWriter.largeSize.x,
+                    h: TextWriter.largeSize.y
+                });
+            }
+            // p - z
+            for (var i = 0; i < 19; i++) {
+                TextWriter.largeCells.push({
+                    x: TextWriter.largeStartPosRow2.x + i * TextWriter.largeIntervalX,
+                    y: TextWriter.largeStartPosRow2.y,
+                    w: TextWriter.largeSize.x,
+                    h: TextWriter.largeSize.y
+                });
+            }
+
+            TextWriter.largeCells.push({ x: 346, y: 164, w: 15, h: 22 });
         }
     }
 
-    write(text: string, x: number, y: number, scale: number = 1) {
+    write(text: string, x: number, y: number, scale: number = 1, large = true) {
         var index: number;
+        var cells = large ? TextWriter.largeCells : TextWriter.smallCells;
         var pos = 0;
         for (var i = 0; i < text.length; i++) {
             var space = false;
@@ -53,7 +84,7 @@
                 continue;
             }
 
-            var image = TextWriter.images[index];
+            var image = cells[index];
             this.context.drawImage(
                 this.spriteSheet,
                 image.x,
